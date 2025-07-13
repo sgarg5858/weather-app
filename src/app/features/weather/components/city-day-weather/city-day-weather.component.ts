a509658b08140f3d674b0484defc21011112de07
Sanjay Garg <sgarg5858@gmail.com>
2025-07-13 16:59:55 +0100

    using use instead of import

--- src/app/features/weather/components/city-day-weather/city-day-weather.component.ts
+++ src/app/features/weather/components/city-day-weather/city-day-weather.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Weather } from '../../interfaces/weather';
-import { DatePipe } from '@angular/common';
 import { WeatherComponent } from '../weather/weather.component';
 
 @Component({
   selector: 'app-city-day-weather',
-  imports: [DatePipe,WeatherComponent],
+  imports: [WeatherComponent],
   templateUrl: './city-day-weather.component.html',
   styleUrl: './city-day-weather.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
